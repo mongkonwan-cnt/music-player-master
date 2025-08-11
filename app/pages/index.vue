cac4079a2c79cd94b42810cceeaf1334e35a595c
mongkonwan-cnt <mongkonwan.num@gmail.com>
2025-08-12 01:53:59 +0700

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>hello word</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// No script logic yet
+</script>
+
+<style scoped>
+/* Add styles here */
+</style>
